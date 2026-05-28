fd80b7a4b4dd80c18c620391b05112633c31f8a2
alexw <alex.wasp2@gmail.com>
2026-05-28 12:45:50 +0200

    feat(spikes): add spike_settings, spike_snapshots, spike_cooldowns, spike_lock to state

--- TGbot/state.py
+++ TGbot/state.py
@@ -1,3 +1,6 @@
+import collections
+import threading
+
 from config import DEFAULT_SETTINGS, DEFAULT_SPOT_SETTINGS, DEFAULT_WALLET
 
 settings = dict(DEFAULT_SETTINGS)
@@ -14,3 +17,14 @@ symbol_volumes = {}
 spot_symbol_volumes = {}
 alert_counts = {"futures": {}, "spot": {}}
 user_state = {}
+
+spike_settings = {
+    "enabled": True,
+    "threshold_pct": 50,
+    "interval_sec": 300,
+    "min_volume_m": 10,
+    "cooldown_min": 30,
+}
+spike_snapshots = collections.deque(maxlen=12)
+spike_cooldowns = {}
+spike_lock = threading.Lock()
